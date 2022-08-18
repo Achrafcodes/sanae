ed7972bb4b8ab564c6cf40177deacc758a70ce1e
achraf essoussy <88954138+Achrafbestcoder@users.noreply.github.com>
2022-08-18 21:14:58 +0100

    Add files via upload

---
+++ projects/index.js
@@ -0,0 +1,11 @@
+const restaurant = {
+  name: 'Acrafs restaurant',
+  location: 'ouled oujih',
+  categorie: ['tacos', 'chawya', 'chicken', 'beef'],
+  starterMenu: ['salad', 'olives', 'grounded beef'],
+  MainMenu: ['chiken', 'burger', 'Spagetti'],
+};
+const arr = [1, 2, 3];
+const [a, b, c] = arr;
+
+console.log(ae, b, c);
